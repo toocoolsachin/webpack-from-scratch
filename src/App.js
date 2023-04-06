755aa3e6ccfc05365495d2a99e6f0f9df24c7fb8
Sachin Kumar <consiku@partner.waters.com>
2023-04-06 11:11:12 +0530

    configuration for optional react import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const App = () => {
   return <h1>Welcome to Webpack tutorial</h1>;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -19,5 +20,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+  ],
   mode: 'development',
 };
